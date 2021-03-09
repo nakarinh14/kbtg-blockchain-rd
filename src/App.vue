4ac7003dd67635d526ce0105d9f3cb59b0b99de2
williampariwat <william.pariwat@gmail.com>
2021-03-09 11:40:35 +0700

    Adding table feature and kbank theme

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="#00E676"
       dark
     >
       <div class="d-flex align-center">
@@ -10,47 +10,41 @@
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          :src="require('@/Logo/kbank.png')"
           transition="scale-transition"
-          width="40"
+          width="150"
         />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+<!--      <v-btn-->
+<!--        href="https://github.com/vuetifyjs/vuetify/releases/latest"-->
+<!--        target="_blank"-->
+<!--        text-->
+<!--      >-->
+<!--        <span class="mr-2">Latest Release</span>-->
+<!--        <v-icon>mdi-open-in-new</v-icon>-->
+<!--      </v-btn>-->
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <DataTable/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
+// import HelloWorld from './components/HelloWorld';
+import DataTable from "@/components/DataTable";
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    // HelloWorld,
+    DataTable,
   },
 
   data: () => ({
---
+++ src/components/DataTable.vue
@@ -0,0 +1,109 @@
+<template>
+
+  <v-data-table
+    :headers="headers"
+    :items="donor"
+    :items-per-page="5"
+    class="elevation-1"
+></v-data-table>
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+      headers: [
+        {
+          text: 'Donor ID',
+          align: 'start',
+          sortable: false,
+          value: 'donor',
+        },
+        { text: 'Recipient ID', value: 'recipient' },
+        { text: 'Category', value: 'type' },
+        { text: 'Amount (฿)', value: 'baht' },
+        { text: 'Tax Reduction', value: 'tax' },
+        { text: 'Date', value: 'date' },
+      ],
+      donor: [
+        {
+          donor: '1103',
+          recipient: 1104,
+          type: "Food",
+          baht: 240,
+          tax: 4.0,
+          date: '02/03/2021',
+        },
+        {
+          donor: '1102',
+          recipient: 1104,
+          type: "Construction",
+          baht: 2400,
+          tax: 168,
+          date: '02/03/2021',
+        },{
+          donor: '1103',
+          recipient: 1104,
+          type: "Food",
+          baht: 240,
+          tax: 4.0,
+          date: '02/03/2021',
+        },{
+          donor: '1103',
+          recipient: 1104,
+          type: "Food",
+          baht: 240,
+          tax: 4.0,
+          date: '02/03/2021',
+        },{
+          donor: '1103',
+          recipient: 1104,
+          type: "Food",
+          baht: 240,
+          tax: 4.0,
+          date: '02/03/2021',
+        },{
+          donor: '1103',
+          recipient: 1104,
+          type: "Food",
+          baht: 240,
+          tax: 4.0,
+          date: '02/03/2021',
+        },{
+          donor: '1103',
+          recipient: 1104,
+          type: "Food",
+          baht: 240,
+          tax: 4.0,
+          date: '02/03/2021',
+        },{
+          donor: '1103',
+          recipient: 1104,
+          type: "Food",
+          baht: 240,
+          tax: 4.0,
+          date: '02/03/2021',
+        },{
+          donor: '1103',
+          recipient: 1104,
+          type: "Food",
+          baht: 240,
+          tax: 4.0,
+          date: '02/03/2021',
+        },{
+          donor: '1103',
+          recipient: 1104,
+          type: "Food",
+          baht: 240,
+          tax: 4.0,
+          date: '02/03/2021',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
